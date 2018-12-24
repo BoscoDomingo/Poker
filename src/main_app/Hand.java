39aeda0bd87f8b912d70b795162b9fbf41813ac5
Bosco Domingo <boscodomingob@gmail.com>
2018-12-24 14:41:15 +0100

    printHand

--- src/main_app/Hand.java
+++ src/main_app/Hand.java
@@ -5,6 +5,23 @@ import java.util.ArrayList;
 public class Hand {
     ArrayList<Card> cards;
 
+    public Hand(ArrayList<Card> cards) {
+        this.cards = cards;
+    }
+
+    public Hand() {
+        this.cards = new ArrayList<>();
+    }
+
+    public void printHand() {
+        System.out.println("Your hand: ");
+        for (Card c : cards) {
+            if (cards.indexOf(c) != cards.size() - 1)
+                System.out.print(c.getNumber() + "" + c.getSuit() + ", ");
+            else System.out.println(c.getNumber() + "" + c.getSuit());
+        }
+    }
+
     public ArrayList<Card> getCards() {
         return cards;
     }
